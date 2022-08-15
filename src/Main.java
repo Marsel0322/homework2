1c2cc3c4431dc3e403faffd66498875af4e99e6f
Argen <kg.alaaai@mail.ru>
2022-08-15 17:15:53 +0600

    домашка22

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-tempurature(33,32);
-tempurature(20,28);
-tempurature(45,25);
-tempurature(31,22);
-tempurature(40,20);
+        System.out.println(tempurature(33,32));
+        System.out.println(tempurature(32,12));
+        System.out.println(tempurature(23,22));
+        System.out.println(tempurature(11,-12));
+        System.out.println(tempurature(33,35));
+
 
     }
     public static String tempurature (int age,int temperature){
